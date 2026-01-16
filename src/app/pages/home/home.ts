d01c6b45b2f2dfd2623e20652843b029c4e7488c
Luis Romero <romeroluis.uno@gmail.com>
2026-01-16 17:35:42 -0300

    Paginador incorporado

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, Signal, signal } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, Signal, signal, ViewChild } from '@angular/core';
 import { PokemonCard } from '../../components/pokemon-card/pokemon-card';
 import { PokemonPicture } from '../../components/pokemon-picture/pokemon-picture';
 import { PokemonService } from '../../services/pokemon.service';
@@ -6,34 +6,49 @@ import { Resultado } from '../../interfaces/pokeapi';
 import { Pokemon } from '../../interfaces/pokemon';
 import { tap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
-  imports: [PokemonCard, PokemonPicture],
+  imports: [PokemonCard, PokemonPicture, MatPaginatorModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  @ViewChild('tarjetas') tarjetasElement!: ElementRef;
   pokemonService = inject(PokemonService);
   listaPokemon = signal<Resultado[]>([]);
   pokemonSeleccionado = signal<Pokemon | undefined>(undefined);
-  pokemonSeleccionadoId = signal<string>(''); // Agregamos esto para comparar instantáneamente
-  pagina: number = 1;
+  pokemonSeleccionadoId = signal<string>('');
+  pageIndex: number = 0;
+  pageSize: number = 10;
+  totalPokemon: number = 1302;
   cargando: boolean = false;
   detalle: boolean = false;
 
   constructor() {
+    this.cargarPokemon(this.pageIndex, this.pageSize);
+  }
+
+  cargarPokemon(pageIndex: number, pageSize: number) {
     this.cargando = true;
+    const paginaServicio = pageIndex + 1;
     this.pokemonService
-      .getByPage(this.pagina)
-      .pipe(tap((data) => this.listaPokemon.set([...this.listaPokemon(), ...data])))
-      .subscribe();
-    this.cargando = false;
-    this.pagina++;
+      .getByPage(paginaServicio, pageSize)
+      .pipe(tap((data) => this.listaPokemon.set(data)))
+      .subscribe(() => {
+        this.cargando = false;
+      });
+  }
+
+  onPaginationChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.cargarPokemon(event.pageIndex, event.pageSize);
   }
 
   async tarjetaClickeada(id: string) {
-    this.pokemonSeleccionadoId.set(id); // Actualizar instantáneamente
+    this.pokemonSeleccionadoId.set(id);
     this.pokemonService.getByIdObs(id).subscribe((data) => {
       this.pokemonSeleccionado.set(data);
     });
