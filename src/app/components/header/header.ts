3db9905dad7d8f06439608cdefa599f5aa239254
Luis Romero <romeroluis.uno@gmail.com>
2026-01-22 16:47:19 -0300

    Proyecto terminado con música incluida. Cosas quizas por hacer: filtro de búsqueda.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MusicPlayer } from "../music-player/music-player";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MusicPlayer],
   templateUrl: './header.html',
   styleUrl: './header.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
-
+  
 }
--- src/app/components/music-player/music-player.ts
+++ src/app/components/music-player/music-player.ts
@@ -1,12 +1,40 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-music-player',
   imports: [],
   templateUrl: './music-player.html',
-  styleUrl: './music-player.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [
+    `
+      .play-button {
+        background: none;
+        border: none;
+        font-size: 24px;
+        cursor: pointer;
+        padding: 10px;
+      }
+    `,
+  ],
 })
 export class MusicPlayer {
+  isPlaying = false;
+  audioSrc = 'assets/music/PokTCGOST2020.mp3';
 
+  togglePlay() {
+    const audio = document.querySelector('audio') as HTMLAudioElement;
+
+    if (this.isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+
+    this.isPlaying = !this.isPlaying;
+  }
+
+  onAudioEnded() {
+    this.isPlaying = false;
+  }
 }
